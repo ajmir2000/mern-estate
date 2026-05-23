89cf5a3a715efa442e8b75d3c53e66a2e6a806c5
Mohammad Ajmir <ajmerfaqiri14.af@gmail.com>
2026-05-23 14:49:01 +0430

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -16,7 +23,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       // requset to the backend say it is post request and send the form data as json
       const res = await fetch("/api/auth/signin", {
         method: "POST",
@@ -26,28 +33,24 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      // requset send to server but server respond with error like user already exist or something else 
+      // requset send to server but server respond with error like user already exist or something else
       // it mean this handle the server error and show the error message to user
-        if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
       // this handle the network error like server is down or something else
-        // it mean handle the frontend error and show the error message to user
-      setLoading(false);
-      setError(error.message);
+      // it mean handle the frontend error and show the error message to user
+      dispatch(signInFailure(error.message));
     }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
-        
         <input
           type="email"
           placeholder="email"
