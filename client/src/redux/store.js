857e55e656ebeb28cf04a629a07a64c5c26ca85b
Mohammad Ajmir <ajmerfaqiri14.af@gmail.com>
2026-05-25 15:23:27 +0430

    I add Comment know better Redux concept

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,22 +1,42 @@
+// configureStore is a function from @reduxjs/toolkit that simplifies the process of creating a Redux store.
+// combineReducers is a function that combines multiple reducers (state) into a single reducing (state) function that you can pass to createStore.
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+//states and actions for user authentication like currentUser, loading and errorfrom userSlice
 import userReducer from "./user/userSlice";
+
+// persistReducer is a function that wraps your root reducer and returns a new reducer that handles the persistence of the state.
+// persistStore is a function that creates a persistor object that can be used to persist the store.
 import { persistReducer, persistStore } from "redux-persist";
+
+// storage is the default storage engine for web, which uses localStorage.
 import storage from "redux-persist/lib/storage";
 
+// this is the root reducer that combines all the reducers in the application, in this case we only have userReducer but we can add more reducers like propertyReducer, etc.
 const rootReducer = combineReducers({
   user: userReducer,
 });
+
+// persistent === save the state in localStorage, so when the user refresh the page or close the browser and open it again, the state will be persisted and the user will not be logged out.
+// this is the configuration object for redux-persist, it tells redux-persist how to persist the state, in this case we are using localStorage and we are giving it a key of "root" which is the key that will be used to store the state in localStorage.
 const persistConfig = {
   key: "root",
   storage,
   version: 1,
 };
+
+// this is the persisted reducer that will be used to create the store and save the state in localStorage, it wraps the rootReducer with the persistReducer function and the persistConfig object.
+// here each update to reducer will be save in localStorage.
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//Here create Redux Store this is like central database for the application where we can store the state of the application and access it from any component in the application, we are using configureStore from @reduxjs/toolkit which simplifies the process of creating a Redux store and it also includes some useful features like middleware and devtools integration, we are also passing the persistedReducer to the store so that the state will be persisted in localStorage, and we are also disabling the serializableCheck middleware because redux-persist uses non-serializable values in the state.
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
+    // redux-persist uses non-serializable values in the state, so we need to disable the serializableCheck middleware to avoid warnings in the console.
     getDefaultMiddleware({ serializableCheck: false }),
 });
 
+//here have responsible for save and reload of sates in localStorage.
 export const persistor = persistStore(store);
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,3 +1,4 @@
+// createSlice is a function from the Redux Toolkit library that simplifies the process of creating a Redux slice, in past wee must manually create action types, action creators and reducers, but with createSlice we can do all of that in one step, it takes an object as an argument that contains the name of the slice, the initial state and the reducers (actions) that will be used to update the state.
 import { createSlice } from "@reduxjs/toolkit";
 
 
@@ -7,6 +8,7 @@ const initialState = {
   loading: false,
 };
 
+//
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -14,6 +16,9 @@ const userSlice = createSlice({
     signInStart: (state) => {
         state.loading = true;
     },
+    // state come from initialState and action is like event have type and payload, type is the name of the action and payload is the data that we want to send to the reducer, in this case we want to send the user data that we get from the server after successful login, so we can update the currentUser state with that data and also set loading to false and error to null.
+
+    // Dispatching an action is like sending a signal to the reducer that something happened and we want to update the state based on that, so when we dispatch signInSuccess action with the user data as payload, the reducer will update the currentUser state with that data and also set loading to false and error to null, so we can use that state in our components to show the user data and also show loading spinner or error message if there is any.
     signInSuccess: (state, action) => {
         state.currentUser = action.payload;
         state.loading = false;
@@ -29,3 +34,43 @@ const userSlice = createSlice({
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
 
 export default userSlice.reducer;
+// redux like simple data like object, array, string not function , promise, date
+
+//reducer === change state function or function get old state and change to new state
+// reducer come get muliple things and return one thing.
+//dispatch mean send an event === action to redux 
+
+// dispatch(action)
+//       ↓
+//  reducer runs
+//       ↓
+//  state updates
+//       ↓
+//  UI updates
+
+
+// خلاصه خیلی ساده:
+
+// Store
+
+// database مرکزی
+
+// Slice
+
+// یک بخش از store
+
+// State
+
+// data فعلی
+
+// Action
+
+// پیغام اتفاق
+
+// Payload
+
+// data همراه action
+
+// Reducer
+
+// منطق تغییر state
