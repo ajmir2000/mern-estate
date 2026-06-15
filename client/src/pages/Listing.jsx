be08b0155e72eca07deb65ceb1acd6ea7217c921
Mohammad Ajmir <ajmerfaqiri14.af@gmail.com>
2026-06-15 12:43:31 +0430

    add image slider to the listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+
+        const data = await res.json();
+
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+
+        setListing(data.listing);
+        setLoading(false);
+        setError(false);
+        console.log(data);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [params.listingId]);
+
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl text-red-700">
+          Something went wrong!
+        </p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    backgroundImage: `url(http://localhost:3000${url.replaceAll("\\", "/")})`,
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                  }}>
+                  {console.log(url)}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
+}
